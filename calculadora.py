efaf4b22906e57c370aca36e101b812305286454
Eduardo_Reis <eduardoazevedo706@gmail.com>
2026-05-23 20:21:12 -0300

    refactor: A função de multiplicação estava mal fatorada

--- calculadora.py
+++ calculadora.py
@@ -2,9 +2,11 @@ def multiplicacao(): #Função de multiplicação entre dois números
     Fator_1 = float(input("Por favor, digite o primeiro fator que você deseja multiplicar: "))
     Fator_2 = float(input("Por favor, digite o segundo fator que você deseja multiplicar: "))
     Produto = Fator_1 * Fator_2
-    print(f"O produto entre {Fator_1} e {Fator_2} é {Produto}")
+    return Fator_1, Fator_2, Produto
 
-multiplicacao()
+Fator_1, Fator_2, Produto = multiplicacao()
+print(f"O produto entre {Fator_1} e {Fator_2} é {Produto}")
+    
 
 
 def divisao():  #Função de divisão entre dois números
