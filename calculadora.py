0c8ced63fcbe51b60811619878bb2917ad554984
Eduardo_Reis <eduardoazevedo706@gmail.com>
2026-05-23 19:54:48 -0300

    feat: Função de multiplicação entre dois números

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+def multiplicacao(): #Função de multiplicação entre dois números
+    Fator_1 = float(input("Por favor, digite o primeiro fator que você deseja multiplicar: "))
+    Fator_2 = float(input("Por favor, digite o segundo fator que você deseja multiplicar: "))
+    Produto = Fator_1 * Fator_2
+    print(f"O produto entre {Fator_1} e {Fator_2} é {Produto}")
+
+multiplicacao()
